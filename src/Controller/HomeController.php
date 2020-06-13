45a87864c0137122d674d3b6c9352d98f9385bdb
rorrotapia <rodrigo.tapia-bravo@hetic.net>
2020-06-13 17:52:17 +0200

    add redirect to admin

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,9 +12,6 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/HomeController.php',
-        ]);
+        return $this->redirectToRoute('easyadmin');
     }
 }
